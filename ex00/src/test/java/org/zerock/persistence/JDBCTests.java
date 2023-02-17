e61306e23cf31c20de68a63ad1f6523581995c15
yang hyunil <fireyang@gmail.com>
2023-02-17 11:04:17 +0900

    JDBC 테스트 코드 수정
    의미는 없지만 초기화 블럭 적용해봄

--- ex00/src/test/java/org/zerock/persistence/JDBCTests.java
+++ ex00/src/test/java/org/zerock/persistence/JDBCTests.java
@@ -11,12 +11,19 @@ import lombok.extern.log4j.Log4j;
 @Log4j
 public class JDBCTests {
 	
+	static {  //초기화 블럭
+		//1.드라이버 로딩
+		try {
+			Class.forName("oracle.jdbc.driver.OracleDriver");
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	
 	@Test
 	public void testConnection(){
 		try {
-			//1.드라이버 로딩
-			Class.forName("oracle.jdbc.driver.OracleDriver");
-			
 			//2.연결해서 커넥션 객체를 가지고 이후 작업직행
 			DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:XE", 
 					"book_ex", "book_ex");
